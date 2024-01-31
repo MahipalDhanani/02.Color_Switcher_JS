85d26fd012f4672aa29ce0595823310d4578620a
MahipalDhanani <141496517+MahipalDhanani@users.noreply.github.com>
2024-01-31 12:30:04 +0530

    files Uploded

---
+++ JS/script.js
@@ -0,0 +1,101 @@
+let head=document.getElementById("head");
+let con=document.getElementById("con");
+// console.log(con);
+
+
+// let btn1=()=>{
+
+//     head.style.backgroundColor="rgb(239, 203, 135)";
+//     head.style.transition="all 1s";
+//     head.style.color="black"
+
+//     con.style.cssText="background-color:rgb(184, 239, 135);transition:all 1s"
+
+// }
+
+// let btn2=()=>{
+
+//     head.style.backgroundColor="rgb(67, 232, 229)";
+//     head.style.transition="all 1s";
+//     head.style.color="";
+
+//     con.style.cssText="background-color:rgb(239, 203, 135);transition:all 1s;"
+
+// }
+
+// let btn3=()=>{
+
+//     head.style.backgroundColor="rgb(255, 0, 0)";
+//     head.style.transition="all 1s";
+//     head.style.color="white";
+
+//     con.style.cssText="background-color:rgb(67, 232, 229);transition:all 1s;"
+
+// }
+
+// let btn4=()=>{
+
+//     head.style.backgroundColor="rgb(164, 235, 102)";
+//     head.style.transition="all 1s";
+//     head.style.color="";
+
+//     con.style.cssText="background-color:rgb(255, 0, 0);transition:all 1s;color:white;"
+
+// }
+
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+
+// let btn=document.getElementsByClassName("boxs");
+
+// for(let i=0; i<btn.length; i++){
+//     btn[i].addEventListener("click",()=>{
+//         if(i==0){
+//             head.style.backgroundColor="rgb(239, 203, 135)";
+//             head.style.transition="all 1s";
+//             head.style.color="black";
+//             con.style.cssText="background-color:rgb(184, 239, 135);transition:all 1s";
+//         }
+//         if(i==1){
+//             head.style.backgroundColor="rgb(67, 232, 229)";
+//             head.style.transition="all 1s";
+//             head.style.color="";
+
+//             con.style.cssText="background-color:rgb(239, 203, 135);transition:all 1s;";
+//         }
+//         if(i==2){
+//             head.style.backgroundColor="rgb(255, 0, 0)";
+//             head.style.transition="all 1s";
+//             head.style.color="white";
+
+//             con.style.cssText="background-color:rgb(67, 232, 229);transition:all 1s;";
+//         }
+//         if(i==3){
+//             head.style.backgroundColor="rgb(164, 235, 102)";
+//             head.style.transition="all 1s";
+//             head.style.color="";
+
+//             con.style.cssText="background-color:rgb(255, 0, 0);transition:all 1s;color:white;";
+//         }
+
+//     })
+// }
+
+
+
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+
+
+function color(color1,color2){
+    head.style.backgroundColor=color1;
+    con.style.backgroundColor=color2;
+}
+
